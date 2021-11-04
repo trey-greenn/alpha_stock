5895167b6715300a5c383d6f3311542713892e7f
trey-greenn <69738260+trey-greenn@users.noreply.github.com>
2021-11-04 15:59:42 -0700

    Add files via upload

---
+++ video_games.py
@@ -0,0 +1,12 @@
+from alpha_vantage.timeseries import TimeSeries
+import streamlit as st
+import pandas as pd
+key = '49FFUFQG9LOCTHRY'
+
+ts = TimeSeries(key, output_format='pandas')
+data = ts.get_monthly_adjusted('MSFT')
+
+
+df = pd.read_csv("C:\more.csv")
+
+st.dataframe(df)
