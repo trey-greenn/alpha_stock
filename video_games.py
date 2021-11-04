6371608579ecdd09083b53d43bb0b8357e413011
trey-greenn <69738260+trey-greenn@users.noreply.github.com>
2021-11-04 16:11:56 -0700

    Update video_games.py

--- video_games.py
+++ video_games.py
@@ -1,12 +1,14 @@
-from alpha_vantage.timeseries import TimeSeries
+#from alpha_vantage.timeseries import TimeSeries
 import streamlit as st
 import pandas as pd
-key = '49FFUFQG9LOCTHRY'
+#key = '49FFUFQG9LOCTHR'
 
-ts = TimeSeries(key, output_format='pandas')
-data = ts.get_monthly_adjusted('MSFT')
+#ts = TimeSeries(key, output_format='pandas')
+#data = ts.get_monthly_adjusted('MSFT')
 
 
 df = pd.read_csv("C:\more.csv")
 
 st.dataframe(df)
+
+#Y
